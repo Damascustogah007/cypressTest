4033b3bd0e398742b7c46a8e993b74666ab82a28
Damascsus Togah <Damascuscharles638@gmail.com>
2022-09-18 16:00:29 +0000

    Docker 4

--- cypress/e2e/Alias/Alias-invoke.cy.js
+++ cypress/e2e/Alias/Alias-invoke.cy.js
@@ -1,35 +1,35 @@
-describe("Alias and Invoke", () => {
-  it("Validate a specific hair care product", () => {
-    cy.visit("https://www.automationteststore.com");
-    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-    cy.get(".fixed_wrapper .prdocutname")
-      .eq(0)
-      // .invoke("text")
-      .as("productThumbNail");
+// describe("Alias and Invoke", () => {
+//   it("Validate a specific hair care product", () => {
+//     cy.visit("https://www.automationteststore.com");
+//     cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+//     cy.get(".fixed_wrapper .prdocutname")
+//       .eq(0)
+//       // .invoke("text")
+//       .as("productThumbNail");
 
-    // cy.get('@productThumbNail').its('length').should('be.gt', 5)
-    cy.get("@productThumbNail").then((name) => {
-      cy.log(name.text());
-    });
+//     // cy.get('@productThumbNail').its('length').should('be.gt', 5)
+//     cy.get("@productThumbNail").then((name) => {
+//       cy.log(name.text());
+//     });
 
-    // it.only("Validate a specific hair care product", () => {
-    //   cy.visit("https://www.automationteststore.com");
-    //   cy.get(".thumbnail").as("productThumbNail");
-    //   cy.get("@productThumbNail").its("length").should(16);
-    //   cy.get(".pricetag jumbotron")
-    //     .children.eq(0)
-    //     .should("have.property", "title")
-    //     .and("contain", "Add to Cart");
-    // });
+//     // it.only("Validate a specific hair care product", () => {
+//     //   cy.visit("https://www.automationteststore.com");
+//     //   cy.get(".thumbnail").as("productThumbNail");
+//     //   cy.get("@productThumbNail").its("length").should(16);
+//     //   cy.get(".pricetag jumbotron")
+//     //     .children.eq(0)
+//     //     .should("have.property", "title")
+//     //     .and("contain", "Add to Cart");
+//     // });
 
-    it.only("Validate a specific hair care product", () => {
-      cy.visit("https://www.automationteststore.com");
-      cy.get(".thumbnail").as("productThumbNail");
-      cy.get("@productThumbNail").its("length").should(16);
-      cy.get(".pricetag jumbotron")
-        .children.eq(0)
-        .should("have.property", "title")
-        .and("contain", "Add to Cart");
-    });
-  });
-});
+//     it.only("Validate a specific hair care product", () => {
+//       cy.visit("https://www.automationteststore.com");
+//       cy.get(".thumbnail").as("productThumbNail");
+//       cy.get("@productThumbNail").its("length").should(16);
+//       cy.get(".pricetag jumbotron")
+//         .children.eq(0)
+//         .should("have.property", "title")
+//         .and("contain", "Add to Cart");
+//     });
+//   });
+// });
